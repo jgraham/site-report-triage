9b31b70e501b0de5d01ff4d5d6e18a7fc3ed339f
James Graham <james@hoppipolla.co.uk>
2024-04-24 17:37:47 +0100

    Ensure that priority and severity fields are loaded before setting bug data

--- bugzilla-content.js
+++ bugzilla-content.js
@@ -6,7 +6,6 @@ const MESSAGES = new Map(Object.entries({
 browser.runtime.onMessage.addListener(async data => {
   if (MESSAGES.has(data.type)) {
     const resp = await MESSAGES.get(data.type)(data);
-    console.log(data, resp);
     return resp;
   }
   throw new Error(`Unrecognised conent script message: ${data.type}`);
@@ -33,24 +32,16 @@ function readBugData(data) {
 async function setBugData(data) {
   for (const id of ["mode-btn", "user-story-edit-btn"]) {
     const editButton = document.getElementById(id);
-    const editable = editButton.disabled || getComputedStyle(editButton).display == "none";
-    console.log(editButton, editable);
-    if (!editable) {
-      let changed = new Promise(resolve => {
-        const observer = new MutationObserver(records => {
-          for (const record of records) {
-            if (record.target === editButton && editButton.disabled) {
-              observer.disconnect();
-              resolve();
-            }
-          }
-        });
-        observer.observe(editButton, {attributes: true});
-      });
+    const editable = elem => elem.disabled || getComputedStyle(elem).display === "none";
+    if (!editable(editButton)) {
       editButton.click();
-      console.log("Awaiting change");
-      await changed;
     }
+    await checkElementState(editButton, editable, {attributes: true}, {timeout: 5000});
+  }
+  for (const id of ["priority", "bug_severity"]) {
+    const select = document.getElementById(id);
+    const selectLoaded = elem => elem.length > 1;
+    await checkElementState(select, selectLoaded, {childList: true, subtree: true}, {timeout: 5000});
   }
 
   document.getElementById("priority").value = data.priority;
@@ -58,4 +49,35 @@ async function setBugData(data) {
   document.getElementById("bug_file_loc").value = data.url;
   document.getElementById("keywords").value = data.keywords;
   document.getElementById("cf_user_story").value = data.userStory;
+}
+
+async function checkElementState(elem, cond, observerOptions, options) {
+  if (cond(elem)) {
+    return;
+  }
+  let changed = new Promise(resolve => {
+    const observer = new MutationObserver(records => {
+      for (const record of records) {
+        if (cond(elem)) {
+          observer.disconnect();
+          resolve();
+          return;
+        }
+      }
+    });
+    observer.observe(elem, observerOptions);
+  });
+  const { timeout } = options;
+  let complete;
+  let timeoutHandle;
+  if (timeout) {
+    let timeoutPromise = new Promise(resolve => {
+      timeoutHandle = setTimeout(resolve, timeout);
+    });
+    complete = Promise.race([changed, timeoutPromise]);
+  } else {
+    complete = changed;
+  }
+  await complete;
+  clearTimeout(timeoutHandle);
 }
